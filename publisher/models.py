a8cd6b4df33a5ad50e6185843bdb0b080d0720f9
richard <ri.oc.torres@outlook.com>
2020-04-22 15:18:07 -0400

    publisher model added

--- publisher/models.py
+++ publisher/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=50)
+    obj = models.Manager()
+
+    def __str__(self):
+        return self.name
+    
