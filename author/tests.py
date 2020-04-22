93228446b607a062890612de838953444d175cc9
richard <ri.oc.torres@outlook.com>
2020-04-22 15:08:58 -0400

    author test get by name, update and delete added with migrate

--- author/tests.py
+++ author/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Author
 
 # Create your tests here.
+
+class AuthorTest(TestCase):
+    def setUp(self):
+        Author.obj.create(name='disney')
+
+    def testGetByName(self):
+        a1 = Author.obj.get(name='disney')
+
+        self.assertEqual(a1.name, 'disney')
+
+    def testUpdate(self):
+        a1 = Author.obj.get(name='disney')
+        a1.name = 'disnei'
+        a1.save()
+
+        a1 = Author.obj.get(name='disnei')
+        self.assertEqual(a1.name, 'disnei')
+
+    def testDelete(self):
+        a1 = Author.obj.get(name='disney')
+        a1.delete()
+
+        a = Author.obj.all().count()
+        self.assertEqual(a, 0)
