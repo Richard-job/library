b9ec3f9adf6f2a4beb377375402df59672b86e7a
richard <ri.oc.torres@outlook.com>
2020-04-22 15:34:02 -0400

    publisher test added

--- publisher/tests.py
+++ publisher/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Publisher
 
 # Create your tests here.
+
+class PublisherTest(TestCase):
+    def setUp(self):
+        Publisher.obj.create(name='disney')
+
+    def testGetByName(self):
+        p1 = Publisher.obj.get(name='disney')
+
+        self.assertEqual(p1.name, 'disney')
+
+    def testUpdate(self):
+        p1 = Publisher.obj.get(name='disney')
+        p1.name = 'dis'
+        p1.save()
+
+        p1 = Publisher.obj.get(name='dis')
+        self.assertEqual(p1.name, 'dis')
+
+    def testDelete(self):
+        p1 = Publisher.obj.get(name='disney')
+        p1.delete()
+
+        p = Publisher.obj.all().count()
+        self.assertEqual(p, 0)
