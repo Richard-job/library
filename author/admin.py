fc79e43a0405c1ce8b2533bf739b1aae559398ee
richard <ri.oc.torres@outlook.com>
2020-04-22 15:01:39 -0400

    author admin added

--- author/admin.py
+++ author/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Author
 
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
+    list_display = ['name', ]
+    search_fields = ['name', ]
+
+admin.site.register(Author, AuthorAdmin)
