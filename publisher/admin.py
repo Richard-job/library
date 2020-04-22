cb4c46536a62647dfeb0c66c33b5d5ae626bc059
richard <ri.oc.torres@outlook.com>
2020-04-22 15:22:43 -0400

    admin added with migrate

--- publisher/admin.py
+++ publisher/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Publisher
 
 # Register your models here.
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
+    list_display = ['name', ]
+    search_fields = ['name', ]
+
+admin.site.register(Publisher, PublisherAdmin)
